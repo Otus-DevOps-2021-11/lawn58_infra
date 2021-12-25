65f2919782ca57d6743d4a75130c9a217402399d
lawn58 <lawn58@github.com>
2021-12-25 13:19:35 +0000

    deleted old scripts

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~
-sudo apt-get install -y git
-git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
-puma -d
-ps aux | grep puma
-ss -tulnp | grep 9292
