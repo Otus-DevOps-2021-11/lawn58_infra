c171a5061be4d22dd7c8b679610f656af2d3a95e
lawn58 <lawn58@github.com>
2022-01-01 19:38:35 +0000

    terraform project commit

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable cloud_id {
+  description = "b1gpcvncb49s9dplpb6o"
+}
+variable folder_id {
+  description = "b1gec5sm6gh6jllu2r6j"
+}
+variable zone {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
+variable public_key_path {
+  description = "~/.ssh/ubuntu.pub"
+}
+variable image_id {
+  description = "fd8bstt380tn356livi1"
+}
+variable subnet_id {
+  description = "e9be7qn5ajoi61siheer"
+}
+variable private_key {
+  description = "~/.ssh/ubuntu"
+}
+variable inst_name {
+  description = "reddit-app"
+}
+
+
