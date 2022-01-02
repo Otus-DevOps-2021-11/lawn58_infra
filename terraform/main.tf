7b6401f06d49c78f0592085a1e62b2b0005da883
lawn58 <47656823+lawn58@users.noreply.github.com>
2022-01-03 00:42:46 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "yandex" {
-  token     = "AQAAAAAHEVvoAATuwbdPijQW9kInhfSKpjpHKcE"
+  token     = "please don't((("
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = var.zone
